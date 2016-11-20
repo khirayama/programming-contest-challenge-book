fed62e39137c112007a8cdbeac6becc47532a9d1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-20 23:19:06 +0900

    Simpify bubble sort

--- algorithm/bubble-sort.js
+++ algorithm/bubble-sort.js
@@ -20,42 +20,19 @@ const items = [3, 9, 6, 5, 1, 2, 4, 0, 7, 8, 2, 6];
   1, 2, 2, 3, false, [0, 1, 2, 3]
 *****************************************/
 
-function swap(items, indexA, indexB) {
-  const items_ = items.concat();
-
-  const temp = items_[indexA];
-  items_[indexA] = items_[indexB];
-  items_[indexB] = temp;
-
-  return items_;
-}
-
-
-function bubbleSort(items) {
-  let items_ = items.concat();
-  for (var i = 0; i < items_.length; i++) {
-    for (var j = 0; j < items_.length - i - 1; j++) {
-      if (items_[j] > items_[j + 1]) {
-        items_ = swap(items_, j, j + 1);
-      }
-    }
-  }
-  return items_;
-}
-
-function _swap(items, indexA, indexB){
+function swap(items, indexA, indexB){
   var temp = items[indexA];
   items[indexA] = items[indexB];
   items[indexB] = temp;
 }
 
-function _bubbleSort(items){
+function bubbleSort(items){
   const len = items.length;
 
   for (let i = 0; i < len; i++) {
     for (let j = 0, stop = len - i; j < stop; j++) {
       if (items[j] > items[j + 1]) {
-        _swap(items, j, j + 1);
+        swap(items, j, j + 1);
       }
     }
   }
@@ -64,4 +41,26 @@ function _bubbleSort(items){
 
 
 console.log('bubble sort:', bubbleSort(items));
-console.log('bubble sort:', _bubbleSort(items.concat()));
+
+// function swap(items, indexA, indexB) {
+//   const items_ = items.concat();
+//
+//   const temp = items_[indexA];
+//   items_[indexA] = items_[indexB];
+//   items_[indexB] = temp;
+//
+//   return items_;
+// }
+//
+//
+// function bubbleSort(items) {
+//   let items_ = items.concat();
+//   for (var i = 0; i < items_.length; i++) {
+//     for (var j = 0; j < items_.length - i - 1; j++) {
+//       if (items_[j] > items_[j + 1]) {
+//         items_ = swap(items_, j, j + 1);
+//       }
+//     }
+//   }
+//   return items_;
+// }
