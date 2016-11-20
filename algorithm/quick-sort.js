e9a2a9dfd371757e636a97fe351d285c44b428c1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-20 23:26:22 +0900

    Clear quick sort

--- algorithm/quick-sort.js
+++ algorithm/quick-sort.js
@@ -1,13 +1,9 @@
 const items = [3, 9, 6, 5, 1, 2, 4, 0, 7, 8, 2, 6];
 
 function swap(items, indexA, indexB) {
-  const items_ = items.concat();
-
-  const temp = items_[indexA];
-  items_[indexA] = items_[indexB];
-  items_[indexB] = temp;
-
-  return items_;
+  const temp = items[indexA];
+  items[indexA] = items[indexB];
+  items[indexB] = temp;
 }
 
 /*****************************************
@@ -18,48 +14,94 @@ function swap(items, indexA, indexB) {
   ave: O(n log n)
 *****************************************/
 function partition(items, leftIndex, rightIndex) {
-  let items_ = items.concat();
-
   const pivotIndex = Math.floor((rightIndex + leftIndex) / 2);
-  const pivot = items_[pivotIndex];
+  const pivot = items[pivotIndex];
 
   let i = leftIndex;
   let j = rightIndex;
 
   while (i <= j) {
-    while (items_[i] < pivot) {
+    while (items[i] < pivot) {
       i += 1;
     }
-    while (items_[j] > pivot) {
+    while (items[j] > pivot) {
       j -= 1;
     }
     if (i <= j) {
-      items_ = swap(items_, i, j);
+      swap(items, i, j);
       i += 1;
       j -= 1;
     }
   }
 
-  return {
-    items: items_,
-    index: i
-  };
+  return i;
 }
 
 function quickSort(items, leftIndex, rightIndex) {
-  const result = partition(items, leftIndex, rightIndex);
-
-  let items_ = result.items;
-  let pivotIndex = result.index;
+  const pivotIndex = partition(items, leftIndex, rightIndex);
 
   if (leftIndex < pivotIndex - 1) {
-    items_ = quickSort(items_, leftIndex, pivotIndex - 1);
+    quickSort(items, leftIndex, pivotIndex - 1);
   }
   if (pivotIndex < rightIndex) {
-    items_ = quickSort(items_, pivotIndex, rightIndex);
+    quickSort(items, pivotIndex, rightIndex);
   }
-  return items_;
+  return items;
 }
 
 
 console.log('quick sort:', quickSort(items, 0, items.length - 1));
+
+// function swap(items, indexA, indexB) {
+//   const items_ = items.concat();
+//
+//   const temp = items_[indexA];
+//   items_[indexA] = items_[indexB];
+//   items_[indexB] = temp;
+//
+//   return items_;
+// }
+//
+// function partition(items, leftIndex, rightIndex) {
+//   let items_ = items.concat();
+//
+//   const pivotIndex = Math.floor((rightIndex + leftIndex) / 2);
+//   const pivot = items_[pivotIndex];
+//
+//   let i = leftIndex;
+//   let j = rightIndex;
+//
+//   while (i <= j) {
+//     while (items_[i] < pivot) {
+//       i += 1;
+//     }
+//     while (items_[j] > pivot) {
+//       j -= 1;
+//     }
+//     if (i <= j) {
+//       items_ = swap(items_, i, j);
+//       i += 1;
+//       j -= 1;
+//     }
+//   }
+//
+//   return {
+//     items: items_,
+//     index: i
+//   };
+// }
+//
+// function quickSort(items, leftIndex, rightIndex) {
+//   const result = partition(items, leftIndex, rightIndex);
+//
+//   let items_ = result.items;
+//   let pivotIndex = result.index;
+//
+//   if (leftIndex < pivotIndex - 1) {
+//     items_ = quickSort(items_, leftIndex, pivotIndex - 1);
+//   }
+//   if (pivotIndex < rightIndex) {
+//     items_ = quickSort(items_, pivotIndex, rightIndex);
+//   }
+//   return items_;
+// }
