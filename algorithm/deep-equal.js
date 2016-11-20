758e7b306b7ebde777555452f7ef0ae7cdb395a4
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-21 07:04:52 +0900

    Create deep equal

---
+++ algorithm/deep-equal.js
@@ -0,0 +1,30 @@
+export function _objEquiv(a, b) {
+  const ka = Object.keys(a);
+  const kb = Object.keys(b);
+
+  if (ka.length !== kb.length) {
+    return false;
+  }
+  ka.sort();
+  kb.sort();
+  for (let i = ka.length - 1; i >= 0; i--) {
+    let key = ka[i];
+    if (!_deepEqual(a[key], b[key])) {
+      return false;
+    }
+  }
+  return typeof a === typeof b;
+}
+
+export function _deepEqual(actual, expected) {
+  if (actual instanceof Date && expected instanceof Date) {
+    return actual.getTime() === expected.getTime();
+  } else if (
+    !actual || !expected ||
+    typeof actual != 'object' && typeof expected != 'object'
+  ) {
+    return actual === expected;
+  } else {
+    return _objEquiv(actual, expected);
+  }
+}
