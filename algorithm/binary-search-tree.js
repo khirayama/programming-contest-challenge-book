f1184cdab3aaad2f2282d1aafd55b52936324451
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-22 07:15:06 +0900

    Implement BinarySearchTree.add

--- algorithm/binary-search-tree.js
+++ algorithm/binary-search-tree.js
@@ -2,7 +2,39 @@ class BinarySearchTree {
   constructor() {
     this._root = null;
   }
-  add() {}
+  add() {
+    const node = {
+      value,
+      left: null,
+      right: null
+    };
+    let current;
+
+    if (this._root === null) {
+      this._root = node;
+    } else {
+      current = this._root;
+      while(true) {
+        if (value < current.value) {
+          if (current.left === null) {
+            current.left = node;
+            break;
+          } else {
+            current = current.left;
+          }
+        } else if (value > current.value) {
+          if (current.right === null){
+            current.right = node;
+            break;
+          } else {
+            current = current.right;
+          }
+        } else {
+          break;
+        }
+      }
+    }
+  }
   contains(value) {
     let found = false;
     let current = this._root;
