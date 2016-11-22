0fde0333b2be144379d76cdf2694165faf120a36
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2016-11-23 06:13:55 +0900

    Update binary-search-tree

--- algorithm/binary-search-tree.js
+++ algorithm/binary-search-tree.js
@@ -51,7 +51,88 @@ class BinarySearchTree {
 
     return found;
   }
-  remove() {}
+  remove(value) {
+    let found = false;
+    let parent = null;
+    let current = this._root;
+
+    while (!found && current) {
+      if (value < current.value) {
+        parent = current;
+        current = current.left;
+      } else if (value > current.value){
+        parent = current;
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+
+    let childCount;
+    let replacement;
+    let replacementParent;
+
+    if (found) {
+      childCount = (current.left !== null ? 1 : 0) + (current.right !== null ? 1 : 0);
+
+      if (current === this._root) {
+        switch (childCount) {
+          case 0:
+            this._root = null;
+            break;
+          case 1:
+            this._root = (current.right === null ? current.left : current.right);
+            break;
+          case 2:
+            replacement = this._root.left;
+            while (replacement.right !== null) {
+              replacementParent = replacement;
+              replacement = replacement.right;
+            }
+            if (replacementParent !== null) {
+              replacementParent.right = replacement.left;
+              replacement.right = this._root.right;
+              replacement.left = this._root.left;
+            } else {
+              replacement.right = this._root.right;
+            }
+            this._root = replacement;
+        }
+      } else {
+        switch (childCount) {
+          case 0:
+            if (current.value < parent.value) {
+              parent.left = null;
+            } else {
+              parent.right = null;
+            }
+            break;
+          case 1:
+            if (current.value < parent.value) {
+              parent.left = (current.left === null ? current.right : current.left);
+            } else {
+              parent.right = (current.left === null ? current.right : current.left);
+            }
+            break;
+          case 2:
+            replacement = current.left;
+            replacementParent = current;
+            while(replacement.right !== null) {
+              replacementParent = replacement;
+              replacement = replacement.right;
+            }
+            replacementParent.right = replacement.left;
+            replacement.right = current.right;
+            replacement.left = current.left;
+            if (current.value < parent.value) {
+              parent.left = replacement;
+            } else {
+              parent.right = replacement;
+            }
+        }
+      }
+    }
+  }
   size() {
     const length = 0;
 
